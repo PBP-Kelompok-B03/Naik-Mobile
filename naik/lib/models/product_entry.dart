fbd2b90792a744c3e26369f95a233a337f7c1a25
Raymundo7532 <woloblocrafael@gmail.com>
2025-12-21 11:59:44 +0700

    Edit and Delete Comments and Replies in product_detail.dart

--- naik/lib/models/product_entry.dart
+++ naik/lib/models/product_entry.dart
@@ -121,6 +121,7 @@ class Comment {
     String commentContent;
     String commentAuthorId;
     String commentAuthorUsername;
+    String commentAuthorRole;
     DateTime commentCreatedAt;
     List<Reply>? replies;
 
@@ -130,6 +131,7 @@ class Comment {
         required this.commentContent,
         required this.commentAuthorId,
         required this.commentAuthorUsername,
+        required this.commentAuthorRole,
         required this.commentCreatedAt,
         this.replies,
     });
@@ -141,6 +143,7 @@ class Comment {
         commentAuthorId: json["comment_author_id"],
         commentAuthorUsername: json["comment_author_username"],
         commentCreatedAt: DateTime.parse(json["comment_created_at"]),
+        commentAuthorRole: json["comment_author_role"] ?? 'user',
         replies: json["replies"] == null ? null : List<Reply>.from(json["replies"].map((x) => Reply.fromJson(x))),
     );
 
@@ -148,7 +151,9 @@ class Comment {
         "comment_id": commentId,
         "comment_rating": commentRating,
         "comment_content": commentContent,
-        "comment_author": commentAuthorId,
+        "comment_author_id": commentAuthorId,
+        "comment_author_username": commentAuthorUsername,
+        "comment_author_role": commentAuthorRole,
         "comment_created_at": commentCreatedAt.toIso8601String(),
         "replies": List<dynamic>.from(replies!.map((x) => x.toJson())),
     };
@@ -159,6 +164,7 @@ class Reply {
     String replyContent;
     String replyAuthorId;
     String replyAuthorUsername;
+    String replyAuthorRole;
     DateTime replyCreatedAt;
 
     Reply({
@@ -166,6 +172,7 @@ class Reply {
         required this.replyContent,
         required this.replyAuthorId,
         required this.replyAuthorUsername,
+        required this.replyAuthorRole,
         required this.replyCreatedAt,
     });
 
@@ -174,6 +181,7 @@ class Reply {
         replyContent: json["reply_content"],
         replyAuthorId: json["reply_author_id"],
         replyAuthorUsername: json["reply_author_username"],
+        replyAuthorRole: json["reply_author_role"] ?? 'user',
         replyCreatedAt: DateTime.parse(json["reply_created_at"]),
     );
 
@@ -181,6 +189,8 @@ class Reply {
         "reply_id": replyId,
         "reply_content": replyContent,
         "reply_author_id": replyAuthorId,
+        "reply_author_username": replyAuthorUsername,
+        "reply_author_role": replyAuthorRole,
         "reply_created_at": replyCreatedAt.toIso8601String(),
     };
 }
